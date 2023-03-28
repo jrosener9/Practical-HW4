6bda14857c58703a7ecbd1030e2849704f9517b4
Vikramaditya Singh <59144331+vikram14s@users.noreply.github.com>
2023-03-27 22:46:35 -0400

    Add files via upload

---
+++ frontend/src/components/ElectionInfoCard.js
@@ -0,0 +1,105 @@
+import { Card, ListGroup, Container } from "react-bootstrap";
+import "chart.js/auto";
+import { Chart } from "react-chartjs-2";
+
+/*
+ * Props:
+ *  - appID (string): id of the election
+ *  - currVotes (list of integers): number of votes for each choice
+ *  - state (JSON): global state of the election
+ */
+function ElectionInfoCard(props) {
+  // list of colors to include in the pie chart
+  const colorList = [
+    "#3181ba",
+    "#632656",
+    "#4dc8e9",
+    "#45134c",
+    "#793ea8",
+    "#5ce3fe",
+    "#4e0d4d",
+    "#2379a8",
+    "#142d6a",
+  ];
+
+  // JSON of data for the pie chart (in the specified format)
+  const data = {
+    labels: props.state["VoteOptions"]
+      ? props.state["VoteOptions"].split(",")
+      : [],
+    datasets: [
+      {
+        label: "Vote Count",
+        data: props.currVotes,
+        backgroundColor: colorList.slice(0, props.currVotes.length),
+        hoverOffset: 4,
+        radius: "75%",
+      },
+    ],
+  };
+
+  /**
+   * Estimate the end date of the election.
+   * @param {number} latestRound - latest round as a number.
+   * @param {number} endRound - end round of the election as a number.
+   * @returns {string} - human readable date as a string.
+   */
+  const estimateEndDate = (latestRound, endRound) => {
+    const secondsPerRound = 4.5;
+
+    let roundToSeconds = (endRound - latestRound) * secondsPerRound;
+    let date = new Date(Date.now());
+    date.setSeconds(roundToSeconds);
+
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}`;
+  }
+
+  /*
+   * Render the card with a list of election info, as well as a pie chart
+   * with the number of votes for each choice.
+   */
+  return (
+    <Card className="h-100">
+      <Card.Body>
+        <Card.Title>Election Info</Card.Title>
+        <ListGroup>
+          <ListGroup.Item>
+            <b>App ID: </b>
+            <a href={"https://testnet.algoexplorer.io/application/" + props.appID} target="_blank" rel="noreferrer">{props.appID}</a>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <b>Creator Address: </b>
+            <a href={"https://testnet.algoexplorer.io/address/" + props.state["Creator"]} target="_blank" rel="noreferrer">{props.state["Creator"] ? props.state["Creator"] : ""}</a>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <b>Most Recent Round: </b>
+            {props.latestRound}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <b>Election End Round: </b>
+            <span className={props.latestRound >= props.state["ElectionEnd"] ? "text-danger" : ""}>{props.state["ElectionEnd"]} </span> 
+            {props.latestRound < props.state["ElectionEnd"] && 
+              <span>(estimated {estimateEndDate(props.latestRound, props.state["ElectionEnd"])})</span>
+            }
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <b>Vote Options:</b> {props.state["VoteOptions"]}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <b> Number of Voters:</b>{" "}
+            {(props.currVotes &&
+              Object.values(props.currVotes).reduce((a, b) => a + b, 0)) ||
+              "0"}
+          </ListGroup.Item>
+        </ListGroup>
+        {props.currVotes.reduce((a, b) => a + b, 0) > 0 && (
+          <Container className="mt-3 px-5">
+            <Chart type="pie" data={data} />
+          </Container>
+        )}
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ElectionInfoCard;
---
+++ smart-contract/deploy.py
@@ -0,0 +1,63 @@
+from algosdk.future import transaction
+from algosdk import account, mnemonic
+from secrets import account_mnemonics
+from election_params import local_ints, local_bytes, global_ints, \
+    global_bytes
+
+# Define keys, addresses, and token
+account_private_keys = [mnemonic.to_private_key(mn) for mn in account_mnemonics]
+account_addresses = [account.address_from_private_key(sk) for sk in account_private_keys]
+
+# Declare application state storage for local and global schema
+global_schema = transaction.StateSchema(global_ints, global_bytes)
+local_schema = transaction.StateSchema(local_ints, local_bytes)
+
+
+def create_app(client, private_key, approval_program, clear_program, global_schema, local_schema, app_args):
+    """
+    Create a new application from the compiled approval_program, clear_program
+    using the application arguments app_args
+    Return the newly created application ID
+    """
+    # TODO: define sender as creator
+    # TODO: declare the on_complete transaction as a NoOp transaction
+    # TODO: get node suggested parameters
+    # TODO: create unsigned transaction
+    # TODO: sign transaction
+    # TODO: send transaction
+    # TODO: await confirmation
+
+    # display results
+    transaction_response = client.pending_transaction_info(tx_id)
+    app_id = transaction_response["application-index"]
+    print("Created new app-id:", app_id)
+
+    return app_id
+
+
+def create_vote_app(client, creator_private_key, election_end, num_vote_options, vote_options):
+    """
+    Create/Deploy the voting app
+    This function uses create_app and return the newly created application ID
+    """
+    # TODO:
+    # Get PyTeal approval program
+    # compile program to TEAL assembly
+    # compile program to binary
+    # Do the same for PyTeal clear state program
+    # TODO: Create list of bytes for application arguments and create new application. 
+
+    app_id = None
+
+    return app_id
+
+
+def main():
+    # TODO: Initialize algod client and define absolute election end time fom the status of the last round.
+    # TODO: Deploy the app and print the global state.
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
